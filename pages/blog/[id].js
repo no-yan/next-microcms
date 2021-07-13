114fdf3f07e873a87ca68e9ee8ca91dbe1e352f4
noyan <1.2.3.up.top.charm00ok001996991@gmail.com>
2021-07-13 18:09:07 +0900

    feat:synctax highlight

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,11 +1,15 @@
 import { client } from "../../libs/client";
 import styles from "../../styles/Home.module.scss";
+import cheerio from "cheerio";
+import hljs from "highlight.js";
+import "highlight.js/styles/hybrid.css";
 
 export default function BlogId({ blog }) {
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>{blog.title}</h1>
       <p className={styles.publishedAt}>{blog.publishedAt}</p>
+      <p className="category">{blog.category && `${blog.category.name}`}</p>
       <div
         dangerouslySetInnerHTML={{ __html: `${blog.body}` }}
         className={styles.post}
@@ -25,10 +29,15 @@ export const getStaticPaths = async () => {
 export const getStaticProps = async (context) => {
   const id = context.params.id;
   const data = await client.get({ endpoint: "blog", contentId: id });
-
+  const $ = cheerio.load(data.body);
+  $("pre code").each((_, elm) => {
+    const result = hljs.highlightAuto($(elm).text());
+    $(elm).html(result.value);
+    $(elm).addClass("hljs");
+  });
   return {
     props: {
-      blog: data,
+      blog: { ...data, body: $.html() },
     },
   };
 };
